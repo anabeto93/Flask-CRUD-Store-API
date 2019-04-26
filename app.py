3feb63d98544068ecca62f0fab0e015d1133fefb
Richard Anabeto Opoku <anabeto93@gmail.com>
2019-04-26 12:28:45 +0000

    added the option to either use Heroku psql or maintain the localhost sqlite3

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, jsonify
 from flask_restful import Api
 from security import authenticate, identity
@@ -9,7 +11,7 @@ from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] =  'JJEK8L4G3BCfY0evXDRxUke2zqAzq6i7wL/Px4SjaEHXqt3x7hsj4+PhVQaH4ujX'
 # app.config['JWT_ACCESS_TOKEN_EXPIRES'] = datetime.timedelta(hours=3)
@@ -17,10 +19,6 @@ app.config['SECRET_KEY'] =  'JJEK8L4G3BCfY0evXDRxUke2zqAzq6i7wL/Px4SjaEHXqt3x7hs
 CORS(app)
 
 api = Api(app)
-
-# @app.before_first_request
-# def create_tables():
-#     db.create_all()
 
 jwt =  JWT(app, authenticate, identity) #/auth
 
